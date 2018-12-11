19bfb2a8ea032a2eec674556ea8ccd7fbe2fc8f5
Stefan Reichert <stefan@stefan-reichert.com>
2018-12-11 21:20:04 +0100

    = 2.8.13 = Renamed readme.txt Updates the update script to v4.4 * YahnisElsts (https://github.com/YahnisElsts/plugin-update-checker)

--- README.txt
+++ README.txt
@@ -3,8 +3,8 @@ Contributors: Stefan Reichert
 Donate link: http://stefan-reichert.com/
 Tags: theme functionality, automatic favicons, additional mime-types, clean header,
 Requires at least: 4.4
-Tested up to: 4.7
-Stable tag: 2.8.12
+Tested up to: 4.8.7
+Stable tag: 2.8.13
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -175,6 +175,12 @@ A. Please create an issue on GitHub: https://github.com/fanfarian/sr-theme-funct
 1. No screenshots available
 
 == Changelog ==
+
+= 2.8.13 =
+Renamed readme.txt
+Updates the update script to v4.4
+* YahnisElsts (https://github.com/YahnisElsts/plugin-update-checker)
+
 
 = 2.8.12 =
 Small bugfixes
--- sr-theme-functionality.php
+++ sr-theme-functionality.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Theme Functionality
  * Plugin URI:        https://github.com/fanfarian/sr-theme-functionality
  * Description:       Functionality plugin with important settings, enhancements and fixes for WordPress themes
- * Version:           2.8.12
+ * Version:           2.8.13
  * Author:            Stefan Reichert
  * Author URI:        http://stefan-reichert.com
  * License:           GNU General Public License v3.0
@@ -71,11 +71,13 @@ function run_sr_theme_functionality() {
 	/**
 	 * Check for updates
 	 */
-	 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 		'https://github.com/fanfarian/sr-theme-functionality/',
 		__FILE__,
 		'sr-theme-functionality'
 	);
+	$myUpdateChecker->setAuthentication('7de8a645aacd93701928766f2cc4f9d18318969a');
+
 	
 	$plugin = new sr_theme_functionality();
 	$plugin->run();
